3b5a08ed416c73fac491da344e0f2e57503dda41
Felipe Esaki Roman <120192433+FelipeEsaki@users.noreply.github.com>
2023-10-27 10:36:36 -0300

    commit teste final

--- script.js
+++ script.js
@@ -22,35 +22,34 @@ function nexImage() {
 }
 
 
+// function atualizarEstilo(contexto) {
+//     img.setAttribute('src', `assets/${contexto}.png`);
 
-function atualizarEstilo(contexto) {
-    img.setAttribute('src', `assets/${contexto}.png`);
+//     btn.forEach(function (contexto) {
+//         contexto.classList.remove('active');
+//     });
+// }
 
-    btn.forEach(function (contexto) {
-        contexto.classList.remove('active');
-    });
-}
+// btnSolicitacao.addEventListener('click', () => {
+//     atualizarEstilo('solicitacao');
 
-btnSolicitacao.addEventListener('click', () => {
-    atualizarEstilo('solicitacao');
+//     btnSolicitacao.classList.add('active');
 
-    btnSolicitacao.classList.add('active');
+//     paragrafo.innerHTML = `O primeiro passo é entrar em contato com nossa equipe e fazer sua solicitação de acordo com sua necessidade. Um de nossos especialistas irá te atender e orientá-lo sobre a melhor linha e as melhores condições no momento, buscando sempre aquela com taxas menores e condições especiais!`;
+// });
 
-    paragrafo.innerHTML = `O primeiro passo é entrar em contato com nossa equipe e fazer sua solicitação de acordo com sua necessidade. Um de nossos especialistas irá te atender e orientá-lo sobre a melhor linha e as melhores condições no momento, buscando sempre aquela com taxas menores e condições especiais!`;
-});
+// btnAnalise.addEventListener('click', () => {
+//     atualizarEstilo('analise');
 
-btnAnalise.addEventListener('click', () => {
-    atualizarEstilo('analise');
+//     btnAnalise.classList.add('active');
 
-    btnAnalise.classList.add('active');
+//     paragrafo.innerHTML = `Depois de pré aprovada, sua solicitação irá para analise dentro da instituição financeira escolhida, passando por especialistas que irão fazer a liberação do crédito.`;
+// });
 
-    paragrafo.innerHTML = `Depois de pré aprovada, sua solicitação irá para analise dentro da instituição financeira escolhida, passando por especialistas que irão fazer a liberação do crédito.`;
-});
+// btnAprovacao.addEventListener('click', () => {
+//     atualizarEstilo('aprovacao');
 
-btnAprovacao.addEventListener('click', () => {
-    atualizarEstilo('aprovacao');
+//     btnAprovacao.classList.add('active');
 
-    btnAprovacao.classList.add('active');
-
-    paragrafo.innerHTML = `Pronto! Sua proposta está aprovada e o crédito será depositado direto na sua conta! Viu como é fácil realizar seu sonho com a OuroInvest? Nossa equipe estará a disposição e irá te auxiliar desde a formalização até o depósito!`;
-})
+//     paragrafo.innerHTML = `Pronto! Sua proposta está aprovada e o crédito será depositado direto na sua conta! Viu como é fácil realizar seu sonho com a OuroInvest? Nossa equipe estará a disposição e irá te auxiliar desde a formalização até o depósito!`;
+// })
